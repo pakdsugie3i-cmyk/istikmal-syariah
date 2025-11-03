b87016770c10c161f75491494cda793e6c504fa0
pakdsugie3i-cmyk <pakdsugie3i@gmail.com>
2025-11-03 17:41:41 +0700

    Update leader.js

--- leader.js
+++ leader.js
@@ -1,1 +1,21 @@
+const leaders = {
+  sugiarto: "6285218453131",
+  agnes: "6285932684440",
+  suwarti: "62881011823140",
+  iyang: "6281388675615",
+  queen: "6282199558169",
+  rahmawan: "6281327498929",
+  regina: "6281285949074",
+  tasya: "966563414575",
+  divawati: "6586210326",
+  tohir: "6281236544821",
+  alpiano: "6282252161911"
+};
 
+function getLeaderInfo() {
+  const params = new URLSearchParams(window.location.search);
+  const leaderKey = params.get("leader");
+  const waNumber = leaders[leaderKey] || leaders.sugiarto; // Default Sugiarto kalau random
+  const leaderName = leaderKey ? leaderKey.charAt(0).toUpperCase() + leaderKey.slice(1) : "Leader Terdekat";
+  return { waNumber, leaderName };
+}
