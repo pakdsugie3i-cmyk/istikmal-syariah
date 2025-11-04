351d916341e88ba2b0d6328e9877213696436947
3ireborn <pakdsugie3i@gmail.com>
2025-11-04 08:14:55 +0700

    Update 11 leader WA

--- leader.js
+++ leader.js
@@ -1,21 +1,14 @@
 const leaders = {
-  sugiarto: "6285218453131",
-  agnes: "6285932684440",
-  suwarti: "62881011823140",
-  iyang: "6281388675615",
-  queen: "6282199558169",
-  rahmawan: "6281327498929",
-  regina: "6281285949074",
-  tasya: "966563414575",
-  divawati: "6586210326",
-  tohir: "6281236544821",
-  alpiano: "6282252161911"
+  "sugiarto": "6285218453131",     // Sugiarto Kurniawan
+  "agnes": "6285932684440",        // Agnes Marsilah
+  "suwarti": "62881011823140",     // Suwarti
+  "iyang": "6281388675615",        // Iyang
+  "queen": "6282199558169",        // Queen
+  "rahmawan": "6281327498929",     // Rahmawan
+  "regina": "6281285949074",       // Regina
+  "tasya": "966563414575",         // Tasya
+  "divawati": "6586210326",        // Divawati
+  "tohir": "6281236544821",        // Tohir
+  "alpiano": "6282252161911",      // Alpiano
+  "default": "6285218453131"       // Default ke Sugiarto (WA masuk ke dia kalau link tanpa ?leader=)
 };
-
-function getLeaderInfo() {
-  const params = new URLSearchParams(window.location.search);
-  const leaderKey = params.get("leader");
-  const waNumber = leaders[leaderKey] || leaders.sugiarto;
-  const leaderName = leaderKey ? leaderKey.charAt(0).toUpperCase() + leaderKey.slice(1) : "Leader Terdekat";
-  return { waNumber, leaderName };
-}
